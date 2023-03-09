9b3b90097f304d7faca0488259cbd8e0ff99bc24
Ian456s <iantang9000@gmail.com>
2023-03-09 12:35:49 -0500

    Added component for home page

---
+++ app/components/Home/index.js
@@ -0,0 +1,5 @@
+import template from './Home.html';
+
+export default {
+    template,
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout';
 import About from '../components/About';
+import Home from '../components/Home';
 // The routes will be managed in this file
 // If there are too many, then the nested routes will be moved to other files within this folder.
 
@@ -7,11 +8,18 @@ const routes = [{
     name: 'main', // Name of route, can be used instead of the path (in case you need to change your path).
     path: '/',
     component: Layout, // The base(?) component of the webpage.
-    children: [{
+    children: [
+        {
         name: 'About',
         path: '/about',
         component: About,
-    }] // Add more routes here.
+        },
+        {
+        name: 'Home',
+        path: '/',
+        component: Home,
+        },
+    ] // Add more routes here.
 }];
 
 export default routes;
