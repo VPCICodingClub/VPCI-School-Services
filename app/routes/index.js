0ed5e3e5de6c0f4cf397fc31fb466fbdc393edb1
Ian456s <iantang9000@gmail.com>
2023-03-09 12:12:07 -0500

    Added about page

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,13 @@ const routes = [{
     name: 'main', // Name of route, can be used instead of the path (in case you need to change your path).
     path: '/',
     component: Layout, // The base(?) component of the webpage.
-    // children: [] // Add more routes here.
+    children: [{
+        name: 'About',
+        path: '/about',
+        component: {
+            template: '<div>about</div>',
+        },
+    }] // Add more routes here.
 }];
 
 export default routes;
