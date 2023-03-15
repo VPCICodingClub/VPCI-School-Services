bca823eabfc79f90ce70369d5088773b7d62964f
MonicaDamyanova <damyanova.monica@gmail.com>
2023-03-14 22:31:23 -0400

    Added some info cards to the clubs page

---
+++ app/components/Clubs/ClubCard/index.js
@@ -0,0 +1,7 @@
+import template from './clubCard.html';
+import './clubCard.css'
+
+export default {
+    template,
+    props: ['name'], // Props are gotten from the parent component.
+};
--- app/components/Clubs/index.js
+++ app/components/Clubs/index.js
@@ -1,5 +1,20 @@
 import template from './clubs.html';
+import ClubCard from './ClubCard';
+import './clubs.css';
 
 export default {
     template,
+    components: {
+        ClubCard,
+    },
+    data() {
+        return {
+            clubs: [
+                { name: 'Name' },
+                { name: 'Name' },
+                { name: 'Name' },
+                { name: 'Name' },
+            ],
+        }
+    },
 };
