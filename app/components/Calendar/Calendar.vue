13df3587afc51b3f529630a76d2111235c2c1a7c
Demeng7215 <demengc6@gmail.com>
2023-06-25 04:05:50 -0400

    Add event calendar

---
+++ app/components/Calendar/Calendar.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {reactive} from 'vue'
+import '@fullcalendar/core'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
+import interactionPlugin from '@fullcalendar/interaction'
+import Washroom from "../Map/Washroom";
+
+const options = reactive({
+  plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+  },
+  editable: false,
+  selectable: true,
+  weekends: true,
+})
+</script>
+
+<template>
+  <div class="calendar-container">
+    <div style="position: relative;">
+      <FullCalendar v-bind:options="options"></FullCalendar>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 1024px;
+  color: #2c3e50;
+  margin: 30px auto;
+}
+</style>
--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,9 +1,11 @@
 import template from './home.html';
-import Map from '../MapFrame';
+import Map from '../Map';
+import Calendar from '../Calendar/Calendar.vue';
 
 export default {
     template,
     components: {
         Map,
+        Calendar,
     },
 };
