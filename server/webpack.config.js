01c84b64a15a4292b5b51c29c0f8249ea1f4ab49
Demeng7215 <demengc6@gmail.com>
2023-06-25 03:59:01 -0400

    Add .vue support

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,50 +1,52 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // Needed otherwise the page says 'Cannot GET /'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {VueLoaderPlugin} = require("vue-loader"); // Needed otherwise the page says 'Cannot GET /'
 
 module.exports = {
-  mode: 'development',
-  entry: './app/index.js', // The entry for the front end.
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.esm-bundler.js',
-      Lib: path.resolve(__dirname, '../app/lib'), // The internal api is put in this folder so that it can be accessed anywhere with the alias.
-      Assets: path.resolve(__dirname, '../assets'),
-    }
-  },
-  output: {
-    filename: '[hash].index.js', // The bundled file made by webpack
-    path: path.resolve(__dirname, '../build'),
-    clean: true,
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      // {
-      //   test: /\.svg$/,
-      //   use: 'vue-svg-inline-loader',
-      // },
+    mode: 'development',
+    entry: './app/index.js', // The entry for the front end.
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+            Lib: path.resolve(__dirname, '../app/lib'), // The internal api is put in this folder so that it can be accessed anywhere with the alias.
+            Assets: path.resolve(__dirname, '../assets'),
+        }
+    },
+    output: {
+        filename: '[hash].index.js', // The bundled file made by webpack
+        path: path.resolve(__dirname, '../build'),
+        clean: true,
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/index.html'
+        }),
+        new webpack.DefinePlugin({ // Vue wants these globals to be defined, otherwise there's a warning.
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false,
+        }),
+        new VueLoaderPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './app/index.html'
-    }),
-    new webpack.DefinePlugin({ // Vue wants these globals to be defined, otherwise there's a warning.
-      __VUE_OPTIONS_API__: true,
-      __VUE_PROD_DEVTOOLS__: false,
-    }),
-  ],
 };
